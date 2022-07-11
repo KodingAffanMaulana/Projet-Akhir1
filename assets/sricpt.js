0c4fd4d7e707d750fe8f2607c0d7722522bc928e
KodingAffanMaulana <affan.maulana2016@students.unila.ac.id>
2022-07-11 19:03:42 +0700

    mm

---
+++ assets/sricpt.js
@@ -0,0 +1,7 @@
+const burgerMenu = document.querySelector('.burger-menu input');
+const nav = document.querySelector('nav ul');
+
+
+burgerMenu.addEventListener('click', function(){
+    nav.classList.toggle('slide');
+})
